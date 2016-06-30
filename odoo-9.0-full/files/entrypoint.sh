c1684bf3d220a8b168145fa60835b53580c2b001
Luis Triana <luistriana.28@gmail.com>
2016-06-29 21:40:33 -0500

    [REF]-Files for odoo 9 and entrypoint for odoo 9

--- odoo-9.0-full/files/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/opt/odoo/odoo.py \
--c /opt/odoo/odoo-server.conf \
---db_user=$DB_ENV_POSTGRES_USER \
---db_password=$DB_ENV_POSTGRES_PASSWORD \
---db_host=$DB_PORT_5432_TCP_ADDR 
-exec "$@"
